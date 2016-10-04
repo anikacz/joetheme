e16a6789b4bf2b6f4bfc87a7f875ac16b61b6084
Anika Czander <anikaczander@gmail.com>
2016-10-04 14:36:55 -0700

    Add name gif and centered to top

---
+++ header-home.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package joetheme
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'joetheme' ); ?></a>
+	<div id="content" class="site-content">
+	<div class="img">
+		<center></center><img src="http://czander.com/wp-content/uploads/2016/10/name.gif" alt="ENTER" width="600" align="middle" class="name"></center>
+	</div>
+	
