76c32bbdeccf7cadf4d73fbedac2143354a3cc10
Anika Czander <anikaczander@gmail.com>
2016-10-12 20:58:34 -0700

    Delete 'proudly powered by' and theme title

--- footer.php
+++ footer.php
@@ -20,9 +20,6 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'joetheme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'joetheme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'joetheme' ), 'joetheme', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
