f0b56c244ffd958b7f273635621a4ecbb49ddcd5
Anika Czander <anikaczander@gmail.com>
2016-10-17 17:24:57 -0700

    Get rid of primary menu button on homepage

--- footer.php
+++ footer.php
@@ -14,7 +14,6 @@
 	</div><!-- #content -->
 
 	<nav id="site-navigation" class="main-navigation" role="navigation">
-		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'joetheme' ); ?></button>
 		<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 	</nav><!-- #site-navigation -->
 
--- functions.php
+++ functions.php
@@ -104,8 +104,6 @@ add_action( 'widgets_init', 'joetheme_widgets_init' );
 function joetheme_scripts() {
 	wp_enqueue_style( 'joetheme-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'joetheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
 	wp_enqueue_script( 'joetheme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
